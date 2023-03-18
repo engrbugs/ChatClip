848f302e615120b98c42ec6449ee522e6d8550f7
engrbugs <engrbugs@gmail.com>
2023-03-17 22:01:46 -0700

    2023-03-17-2201

--- ChatClip.cpp
+++ ChatClip.cpp
@@ -2,11 +2,99 @@
 //
 
 #include <iostream>
+#include <iostream>
+#include <string>
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+#include <iostream>
+#include <map>
+#include <optional>
+#include <vector>
+#include <thread>
+
+#include <thread>
+#include <mutex>
+
+std::mutex io_mutex;
+
+
+using namespace std;
+
+const map<string, vector<string>> COMMANDS = {
+    {"Cover Letter", {"cover", "c"}},
+    {"Exit", {"exit", "x"}},
+};
+
+const string VER = "0.6b";
+
+
+void output_thread()
+{
+    while (true)
+    {
+        lock_guard<mutex> lock(io_mutex);
+        cout << endl << "Output thread is running" << endl;
+        cout << "main: ";
+        this_thread::sleep_for(chrono::seconds(3)); // Delay for 3 second
+    }
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<string> triggers;
+
+    cout << "List of commands: " << VER << endl;
+
+    for (auto& x : COMMANDS) {
+        cout << x.first << ": [" << x.second[1] << "]" << x.second[0] << endl;
+        triggers.push_back(x.second[0]);
+        triggers.push_back(x.second[1]);
+    }
+    string input_str;
+    auto& it = *new vector<string>::iterator(triggers.end());
+
+    std::thread t(output_thread);
+    while (it == triggers.end()) {
+        cout << "main: ";
+        if (!getline(cin, input_str)) {
+            // handle error or end-of-input
+            return 1;
+        }
+        // process the input_str as needed
+        it = find(triggers.begin(), triggers.end(), input_str);
+    }
+    // do something with it, e.g. print the matched string
+    
+    delete& it;
+    return 0;
 }
+
+//
+//int main()
+//{
+//    std::thread t(output_thread);
+//    std::this_thread::sleep_for(std::chrono::seconds(1)); // Delay for 1 second
+//
+//    while (true)
+//    {
+//        std::lock_guard<std::mutex> lock(io_mutex);
+//        std::cout << "Enter input: ";
+//        std::string input;
+//        std::cin >> input;
+//        std::cout << "You entered: " << input << std::endl;
+//        std::this_thread::sleep_for(std::chrono::seconds(3)); // Delay for 1 second
+//    }
+//
+//    return 0;
+//}
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
